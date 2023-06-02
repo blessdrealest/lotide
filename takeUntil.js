55d39c840ea69e4ed543e86056dfdbda33599259
Blessing <dijiblessing2@hotmail.co.uk>
2023-06-02 15:10:18 -0600

    copied in assertArraysEqual and equalArrays tests

--- takeUntil.js
+++ takeUntil.js
@@ -11,3 +11,31 @@ const takeUntil = function(array, callback) {
   }
   return result;
 };
+
+
+
+
+//assertArraysEqual test
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2))  {
+    console.log(`🙂 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`😞Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+//the arrays need to be of the same length to be equal
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) { //this will check to see that the 2 arrays have the same elements
+      return false;
+    }
+  }
+
+  return true;
+  // this will conirm that all elements are the same if true
+}
